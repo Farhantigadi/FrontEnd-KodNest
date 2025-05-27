9eb20df2dcf730317fd3d4e766b86374ca9034f8
Farhantigadi <farhantigadi123@gmail.com>
2025-05-27 20:36:58 +0530

    js - let vs const vs var more eg

--- kodnest-js/basic-js/letvarkey/script.js
+++ kodnest-js/basic-js/letvarkey/script.js
@@ -18,3 +18,16 @@ function testVarLetConst() {
 }
 
 testVarLetConst();
+
+function showScope() {
+    let city = "Mumbai";   
+    var country = "India"; 
+    
+    console.log("City:", city);      
+    console.log("Country:", country); 
+}
+
+showScope();
+
+console.log("Outside function, City:", city)
+console.log("Outside function, Country:", country);
